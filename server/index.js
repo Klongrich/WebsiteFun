4d3c51ceae351778abfda90f7d799c588c150cc5
Kyle Longrich <longrichk@gmail.com>
2020-04-21 18:02:26 -0400

    added broswer tracking

--- server/index.js
+++ server/index.js
@@ -4,16 +4,19 @@ const port = 3000;
 
 const expressip = require('express-ip');
 const cors = require('cors');
+const useragent = require('express-useragent');
 
 app.use(cors());
 app.use(expressip().getIpInfoMiddleware);
+app.use(useragent.express());
 
 app.get('/', (req, res) => {
   const ipInfo = req.ipInfo;
+  const browserInfo = req.useragent;
 
-  var city = ipInfo.city;
+  var browser = `On: ${browserInfo.browser}, Version: ${browserInfo.version}, OS: ${browserInfo.os}`;
+  var message = `Broswing from ${ipInfo.city}, ${ipInfo.country} ${browser}`;
 
-  var message = `Broswing from ${city}, ${ipInfo.country}`;
   res.send(message);
 })
 
