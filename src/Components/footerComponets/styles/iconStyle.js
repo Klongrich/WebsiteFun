73ca930eef7d25531bae667dbda76357b62ba416
Kyle Longrich <longrichk@gmail.com>
2020-08-26 06:13:47 -0400

    fixed footer on moblie

--- src/Components/footerComponets/styles/iconStyle.js
+++ src/Components/footerComponets/styles/iconStyle.js
@@ -7,28 +7,14 @@ export const IconStyleWrapper = styled.div`
 
 text-align: center;
 background-color:black;
-
-
-@media (min-width: 1024px) {
-
-  ${StyledIconBase} {
+  
+${StyledIconBase} {
       height: 35px;
       width: 35px;
       padding-left: ${px2vw(50)};
       padding-right: ${px2vw(50)};
       margin-top: ${px2vw(42)};
       margin-bottom: ${px2vw(42)};
-    }
-  }
-
-  @media only screen and (min-width: 960px) {
-    /* styles for browsers larger than 960px; */
-  }
-  @media only screen and (min-width: 1440px) {
-    /* styles for browsers larger than 1440px; */
-  }
-  @media only screen and (min-width: 2000px) {
-    /* for sumo sized (mac) screens */
   }
 
   @media only screen and (max-device-width: 480px) {
@@ -44,19 +30,6 @@ background-color:black;
       padding-right: ${px2vw(62)};
 
     }
-  }
-
-  @media only screen and (device-width: 768px) {
-   /* default iPad screens */
-  }
-
-  /* different techniques for iPad screening */
-  @media only screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait) {
-    /* For portrait layouts only */
-  }
-
-  @media only screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape) {
-    /* For landscape layouts only */
   }
 `
 
