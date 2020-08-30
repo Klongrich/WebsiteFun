a596e0a2cb82177b908e123c08d4293025d7d01a
Kyle Longrich <longrichk@gmail.com>
2020-08-29 20:45:26 -0400

    started stocks page, will refactor later

--- src/Components/pages/Stocks/stocksHomePage.js
+++ src/Components/pages/Stocks/stocksHomePage.js
@@ -4,6 +4,17 @@ import styled from "styled-components";
 import teslaPic from './TeslaChart2.png'
 import candleStick from './CandelStick.png'
 
+import macd from './MACD.png'
+import rsi from './RSI.png'
+import stoch from './STOCH.png'
+import coppock from './CoppuckCurve.png'
+import atr from './ATR.png'
+
+import stochForumal from "./stochForumal.png"
+import stochExample from "./stochExample.png"
+
+import level2 from './level2.jpg'
+
 export const Header = styled.div`
  
     border: 3px black solid;
@@ -35,11 +46,11 @@ export const TopPage = styled.div`
     border: 1px black solid;
 
     padding-left: 40px;
+    padding-bottom: 10px;
 
     background-color: #AFD275;
 
     font-family: Helvetica;
-
 
     p {
     
@@ -49,7 +60,7 @@ export const TopPage = styled.div`
   
 `
 
-export const BasicInformation = styled.div`
+export const HowToReadAChart= styled.div`
     
     border: 1px black solid;
   
@@ -68,6 +79,7 @@ export const BasicInformation = styled.div`
 
 
     padding-left: 20px;
+    padding-bottom: 10px;
 
     background-color: #C2CDA0;
 
@@ -75,10 +87,9 @@ export const BasicInformation = styled.div`
 
 `
 
-export const Terminology = styled.div`
+export const BasicTerminology = styled.div`
     
     border: 1px black solid;
-
 
     ul {
         list-style-type: none;
@@ -93,12 +104,118 @@ export const Terminology = styled.div`
     }
 
     padding-left: 20px;
+    padding-bottom: 10px;
+
+    background-color: #C2B9B0;
+
+    font-family: Helvetica;
+`
+
+
+
+export const BasicInformation = styled.div`
+    
+    border: 1px black solid;
+
+    ul {
+        list-style-type: none;
+    }
+
+    li {
+        font-weight: bold;
+    }
+
+    p {
+        margin-left: 30px;
+    }
+
+    padding-left: 20px;
+    padding-bottom: 10px;
+
+    
+    background-color: #C2CDA0;
+
+    font-family: Helvetica;
+
+
+`
+
+export const Indicators = styled.div`
+    
+    border: 1px black solid;
+
+    ul {
+        list-style-type: none;
+    }
+
+    li {
+        font-weight: bold;
+    }
+
+    p {
+        margin-left: 30px;
+    }
+
+    padding-left: 20px;
+    padding-bottom: 10px;
 
     background-color: #C2B9B0;
 
     font-family: Helvetica;
 
-    height: 400px;
+
+`
+
+export const LevelTwoData = styled.div`
+    
+    border: 1px black solid;
+
+    ul {
+        list-style-type: none;
+    }
+
+    li {
+        font-weight: bold;
+    }
+
+    p {
+        margin-left: 30px;
+    }
+
+    padding-left: 20px;
+    padding-bottom: 10px;
+
+    background-color: #C2CDA0;
+
+    font-family: Helvetica;
+
+
+`
+
+export const Conclusion = styled.div`
+    
+    border: 1px black solid;
+
+    ul {
+        list-style-type: none;
+    }
+
+    li {
+        font-weight: bold;
+    }
+
+    p {
+        margin-left: 30px;
+    }
+
+    padding-left: 20px;
+    padding-bottom: 10px;
+
+    background-color: #C2B9B0;
+
+    font-family: Helvetica;
+
+
 `
 
 export const TeslaChart = styled.div`
@@ -132,15 +249,14 @@ export default function stocksHomePage () {
               stock market if you chose to invest and / or day trade.
            </p>
 
-            <h2> Rule One: </h2>
-                <p> Quit looking for hot picks</p>
-
+           <h2>Rule One: </h2>
+                <p> Stop looking for hot picks</p>
         </TopPage>
 
-        <BasicInformation>
+        <HowToReadAChart>
             <h1>How To Read A Chart</h1>
 
-    <div Style="float: left; margin-left: 200px">
+        <div Style="float: left; margin-left: 200px">
             <h2 Style="text-align: center">Candel Stick</h2>
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Candlestick_chart_scheme_03-en.svg/1024px-Candlestick_chart_scheme_03-en.svg.png"
                  height="250px" 
@@ -173,7 +289,7 @@ export default function stocksHomePage () {
                     selected time frame is what applys to the measurments above.
                 </p>
         </ul>
-        </BasicInformation>
+        </HowToReadAChart>
 
         <h2 Style="text-align: center;">Chart Example (Timeframe - 1 Day Candels) </h2>
         <TeslaChart>
@@ -181,46 +297,113 @@ export default function stocksHomePage () {
         </TeslaChart>
 
 
-        <Terminology>
-            <h1>Terminology</h1>
+        <BasicTerminology>
+            <h1>Basic Terminology</h1>
             
             <ul Style="float: left">
                 <li>MarketCap: </li> <p>Total amount the company is worth</p>
                 <li>Volume: </li> <p> Total amount of shares being bought and sold</p>
                 <li>Float: </li> <p>Amount of Shares that are aviable to be tradded publicly</p>
-                <li>EPS: </li> <p>Earings Per Share.</p>
+                <li>Short </li> <p>Taking a negative position on a stock (betting it will go down)</p>
             </ul>
 
-            <ul Style="display: inline-block">
-                <li>Options</li>
-                <li>Equites</li>
-                <li>Dirviates</li>
-                <li>Forex</li>
-                <li>Comodittes</li>
-                <li>Futures</li>
+            <ul Style="display: inline-block; margin-left: 50px;">
+                <li>Options</li> <p>  Derivatives based on the value of underlying securities such as stocks. </p>
+                <li>Equites</li> <p> Shares of value issued by a companies. </p>
+                <li>Forex</li> <p> Marketplace where various national currencies are traded</p>
+                <li>Comodittes</li> <p>Materials or primary agricultural products such as copper or coffee.</p>
             </ul>
-        </Terminology>
+            </BasicTerminology>
 
 
 
         <BasicInformation>
             <h1>Basic Information</h1>
 
-            <ul>
-                <li>Brokerages</li>
-                    <ul>
-                        <li>Interactive Brokers</li>
-                        <li>E-Trade</li>
-                        <li>Robin Hood</li>
-                        <li>Light Speed</li>
-                    </ul>
-                <li>How To Place A Trade</li>
-                <li>Indicators</li>
-                <li>Overlays</li>
-                <li>Level 2 data</li>
-            </ul>
-        </BasicInformation>
+            <ul Style="float: left; padding-bottom: 10px;">
+                <li>US Brokerages</li> 
+                        <li Style="padding-bottom: 20px; padding-top: 20px;"> 1.) Interactive Brokers</li>
+                        <li Style="padding-bottom: 20px;"> 2.) E-Trade</li>
+                        <li Style="padding-bottom: 20px;"> 3.) Robin Hood</li> 
+                        <li Style="padding-bottom: 20px;"> 5.) Charels Scwhab </li>
+                        <li Style="padding-bottom: 20px;"> 6.) TD Ameritrade</li>
+            </ul> 
 
+                    <ul Style="display: inline-block; margin-left: 200px">
+                        <li>How To Place A Trade</li> <br />
+                        <li>Limit Order</li> <p>Order place to buy at or below / sell at or above a certain price (preferred)</p>
+                        <li>Marekt Order</li> <p>Buys or Sells at current going market rate</p>
+                        <li>Stop Lose</li> <p>Placed to sell a stock or cover a short at a certain price</p>
+                    </ul>
+       
+
+        </BasicInformation> 
+
+
+        <Indicators>
+            <h1> Indicators </h1>
+                <ul>
+
+                <div Style="border: 3px solid grey; 
+                            margin-left: -62px;
+                            padding-left: 30px;
+                            padding-top: 30px;
+                            padding-bottom: 30px;
+                            margin-bottom: 20px; 
+                            width: 1500px;"
+                            >
+                    <li> STOCH: </li>
+                        <img Style="margin-top: 30px;
+                                    margin-bottom: 30px;
+                                    margin-left: 50px;
+                                    border: 2px black solid;" src={stoch} />
+                    <li>Formula: </li>
+                        <img  Style="margin-top: 30px;
+                                    margin-bottom: 30px;
+                                    margin-left: 50px;
+                                    border: 2px black solid;" src={stochForumal} />
+                    <li>Explained:</li>
+                        <p>H = The "High" of the time period</p>
+                        <p>L = The "Low" of the time period</p>
+                        <p>C = The "Close" of the time period</p>
+                    <li>Code Example: </li>
+                        <img  Style="margin-top: 30px;
+                                    margin-bottom: 30px;
+                                    margin-left: 50px;
+                                    border: 2px black solid;" src={stochExample} />
+                    <li>Link to code: https://github.com/Klongrich/ChartingSoftware/blob/master/src/indicators/listtwo.java</li>
+
+                </div>
+
+                    <li> MACD </li>
+                        <img src={macd}  />
+                    <li> ATR </li>
+                        <img src={atr} />
+                    <li> RSI </li>
+                        <img src={rsi} />
+                    <li> Coppock Curve </li>
+                        <img src={coppock} />
+
+                   <li> And Many More .....</li>
+                </ul>
+        </Indicators>
+
+        <BasicInformation>
+            <h1> Level 2 Data</h1>
+            <div Style="margin-left: 350px; margin-bottom: 20px;">
+                <p Style="font-weight: bold; padding-left: 120px">(Example of Level 2 data)</p>
+                <img Style="border: 2px black solid; width: 500px;" src={level2} />
+            </div>
+            <ul Style="float: left; margin-left: 200px; margin-top:">
+                <li>Bid: </li> <p>Orders That are placed to buy</p>
+                <li>Ask: </li> <p>Order that are placed to sell </p>
+            </ul>
+            <ul Style="display: inline-block; margin-left: 200px;">
+                <li>ID: </li> <p>Exchange your are order is being routed to</p>
+                <li>Size: </li> <p>Amount of shares for sale or purchase at that price</p>
+            </ul>
+
+        </BasicInformation>
  
         </>
     )
