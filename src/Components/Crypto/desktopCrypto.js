610e82aeb75fd1c0e07318442c93d64f5caec465
Kyle Longrich <longrichk@gmail.com>
2021-02-09 20:03:28 -0500

    crypto blog link update

--- src/Components/Crypto/desktopCrypto.js
+++ src/Components/Crypto/desktopCrypto.js
@@ -1,162 +1,148 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import copy from "copy-to-clipboard";
 import { useHistory } from "react-router-dom";
-import Footer from '../footerComponets/Footer'
-import {PageContent} from './blogpage'
-import {Share} from '@styled-icons/boxicons-regular/Share'
-import {mainPost, secondPost, thridPost, AllPost} from './data/cryptoBlogData'
-
-var URLlink = "https://www.longrichk.com/Crypto?articleID="; 
+import Footer from "../footerComponets/Footer";
+import { PageContent } from "./blogpage";
+import { Share } from "@styled-icons/boxicons-regular/Share";
+import {
+  mainPost,
+  secondPost,
+  thridPost,
+  AllPost,
+} from "./data/cryptoBlogData";
 
 export const Container = styled.div`
-
-    background-color: #759ce6;
-    border: 1px black #525252;
-    height: 610px;
-
-`
+  background-color: #759ce6;
+  border: 1px black #525252;
+  height: 610px;
+`;
 
 export const Header = styled.div`
+  background-color: #20448a;
+  color: white;
 
-    background-color: #20448a;
-    color: white;
+  height: 60px;
+  border: 1px #525252 solid;
 
-    height: 60px;
-    border: 1px #525252 solid;
+  margin-top: -10px;
 
-    margin-top: -10px;
+  h2 {
+    margin-left: 15px;
+  }
 
-    h2 { 
-        margin-left: 15px;
-    }
-
-    p {
-        text-align: right;
-        margin-top: -45px;
-        margin-right: 30px;
-    }
-
-`
+  p {
+    text-align: right;
+    margin-top: -45px;
+    margin-right: 30px;
+  }
+`;
 
 export const BlogPostTitles = styled.div`
+  background-color: black;
+  color: white;
 
-    background-color: black;
-    color: white;
+  opacity: 0.7;
 
-    opacity: 0.7;
+  padding-top: 1px;
 
-    padding-top: 1px;
-    
-    width: 100%;
+  width: 100%;
 
-    font-size: ${props => props.fontSize};
+  font-size: ${(props) => props.fontSize};
 
-    position: absolute; 
-    bottom: 0;
-
-`
+  position: absolute;
+  bottom: 0;
+`;
 
 export const BlogPost = styled.div`
+  posistion: realtive;
 
-    posistion: realtive; 
+  text-align: center;
+  width: ${(props) => props.width};
 
-    text-align: center;
-    width: ${props => props.width};
+  height: ${(props) => props.height};
+  border: 1px black solid;
+  border-radius: 5px;
 
-    height: ${props => props.height};
-    border: 1px black solid;
-    border-radius: 5px;
+  margin-left: ${(props) => props.marginLeft};
+  margin-top: 3%;
 
-    margin-left: ${props => props.marginLeft};
-    margin-top: 3%;
+  background-image: url(${(props) => props.Image});
+  background-size: 100% 100%;
 
-    background-image: url(${props => props.Image});
-    background-size: 100% 100%;
+  float: left;
 
-    float: left;
+  &:hover {
+    transition-timing-function: ease-in;
+    transition: 0.2s;
+    transform: scale(1.05);
+  }
 
-    &:hover {
-        transition-timing-function: ease-in;
-        transition: 0.2s;
-        transform: scale(1.05);
-      }
-    
-      transition-timing-function: ease-out;
-      transition: 0.25s;
-      transform: scale(1);
-`
+  transition-timing-function: ease-out;
+  transition: 0.25s;
+  transform: scale(1);
+`;
 
 export const ConfrimCopyText = styled.div`
+  visibility: ${(props) => props.show};
+  border: 1px solid black;
+  border-radius: 5px;
+  width: 200px;
 
-    visibility: ${props => props.show};
-    border: 1px solid black;
-    border-radius: 5px;
-    width: 200px;
+  background-color: grey;
 
-    background-color: grey;
+  margin-bottom: 20px;
+`;
 
-    margin-bottom: 20px;
+export default function DesktopCrypto() {
+  const [veiwingPage, setVeiwingPage] = useState(false);
 
+  const [pageTitle, setPageTitle] = useState("Title");
+  const [pageDate, setPageDate] = useState("Date");
+  const [pageContent, setPageContent] = useState(mainPost.content);
 
-`
+  const [showLinkCopied, setShowLinkCopied] = useState("hidden");
 
-export default function DesktopCrypto () {
+  useEffect(() => {
+    var Temp = window.location.href.split("=");
+    var id = Temp[1];
 
-    const [veiwingPage, setVeiwingPage] = useState(false);
-
-    const [pageTitle, setPageTitle] = useState("Title");
-    const [pageDate, setPageDate] = useState("Date");
-    const [pageContent, setPageContent] = useState(mainPost.content);
-
-    const [showLinkCopied, setShowLinkCopied] = useState("hidden");
-
-
-    useEffect(() => {
-
-        var Temp = window.location.href.split("=");
-        var id = Temp[1];
-
-        if (id) {
-            updatePageById(parseInt(id, 10))
-        }
-    }, [])
-
-    function updatePageById(id){
-       
-        var update = AllPost.map(function (data) {
-            if (data.id == id) {
-
-                setPageTitle(data.title);
-                setPageDate(data.date);
-                setPageContent(data.content);
-                
-                setVeiwingPage(true);
-                window.scrollTo(0, 0)
-     
-              }    
-            });
-
-        return (update);
+    if (id) {
+      updatePageById(parseInt(id, 10));
     }
+  }, []);
 
-    function CopyText (text) {
-        copy(text);
-        setShowLinkCopied("visible");
-    }
+  function updatePageById(id) {
+    var update = AllPost.map(function (data) {
+      if (data.id == id) {
+        setPageTitle(data.title);
+        setPageDate(data.date);
+        setPageContent(data.content);
 
-    if (veiwingPage) {
-        return (
-            <>
-            <Header>
-                <h2> Crypto Blog </h2>
-                <p> V1 </p>
-            </Header>
+        setVeiwingPage(true);
+        window.scrollTo(0, 0);
+      }
+    });
 
-            <PageContent title={pageTitle} date={pageDate} content={pageContent}/>
+    return update;
+  }
 
+  function CopyText(text) {
+    copy(text);
+    setShowLinkCopied("visible");
+  }
 
-            {/*
+  if (veiwingPage) {
+    return (
+      <>
+        <Header>
+          <h2> Crypto Blog </h2>
+          <p> V1 </p>
+        </Header>
+
+        <PageContent title={pageTitle} date={pageDate} content={pageContent} />
+
+        {/*
             <div Style="margin-top: 50px;
                         border: 1px solid black;
                         color: white;
@@ -185,73 +171,62 @@ export default function DesktopCrypto () {
 
             </div>
             */}
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Header>
+          <h2> Crypto Blog </h2>
+          <p> V1 </p>
+        </Header>
 
+        <Container>
+          <BlogPost
+            Image={mainPost.image}
+            width="40%"
+            height="340px"
+            marginLeft="8%"
+            onClick={() => (window.location = "?articleID=" + mainPost.id)}
+          >
+            <BlogPostTitles>
+              <h2>DeFi and It's Current State</h2>
+              <p> {mainPost.date} </p>
+            </BlogPostTitles>
+          </BlogPost>
 
-            </>
-        );
-    } else {
-        return (
-            <>
-            <Header>
-                <h2> Crypto Blog </h2>
-                <p> V1 </p>
-            </Header>
-
-            <Container>
-            <BlogPost Image={mainPost.image}
-                      width="40%"
-                      height="340px"
-                      marginLeft="8%"
-                      onClick={() => 
-                        window.location = URLlink + mainPost.id
-                      } 
-                      >
-                <BlogPostTitles> 
-                    <h2>DeFi and It's Current State</h2>
-                    <p> {mainPost.date} </p>
-                </BlogPostTitles>
+          {secondPost.map((data) => (
+            <BlogPost
+              Image={data.image}
+              width="35%"
+              height="150px"
+              marginLeft="8%"
+              onClick={() => (window.location = "?articleID=" + data.id)}
+            >
+              <BlogPostTitles fontSize="14px">
+                <h2>{data.title}</h2>
+                <p>{data.date}</p>
+              </BlogPostTitles>
             </BlogPost>
-    
-            {secondPost.map( data => (
-                
-                <BlogPost Image={data.image} 
-                          width="35%"
-                          height="150px"
-                          marginLeft="8%"
-                          onClick={() =>
-                            window.location = URLlink + data.id
-                        } >
-    
-                    <BlogPostTitles fontSize="14px"
-                                    >
-                        <h2>{data.title}</h2>
-                        <p>{data.date}</p>
-                    </BlogPostTitles>
-    
-                </BlogPost>
-            ))}
-    
-            {thridPost.map( data => (
-                <BlogPost Image={data.image} 
-                          width="15%"
-                          height="150px"
-                          marginLeft="8%"
-                          onClick={() =>                           
-                              window.location = URLlink + data.id
-                            }>
-    
-                    <BlogPostTitles fontSize="12px"
-                                    >
-                        <h2>{data.title}</h2>
-                        <p>{data.date}</p>
-                    </BlogPostTitles>
-    
-                </BlogPost>
-            ))}
-    
-            </Container>
+          ))}
+
+          {thridPost.map((data) => (
+            <BlogPost
+              Image={data.image}
+              width="15%"
+              height="150px"
+              marginLeft="8%"
+              onClick={() => (window.location = "?articleID=" + data.id)}
+            >
+              <BlogPostTitles fontSize="12px">
+                <h2>{data.title}</h2>
+                <p>{data.date}</p>
+              </BlogPostTitles>
+            </BlogPost>
+          ))}
+        </Container>
         <Footer />
-                </>
-        );
-    }
+      </>
+    );
+  }
 }
--- src/Components/Crypto/moblieCrypto.js
+++ src/Components/Crypto/moblieCrypto.js
@@ -1,163 +1,146 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
-import Footer from '../footerComponets/Footer'
+import Footer from "../footerComponets/Footer";
 
-import {PageContent} from './blogpage'
+import { PageContent } from "./blogpage";
 
-import {AllPost} from './data/cryptoBlogData'
-
-var URLlink = "https://www.longrichk.com/Crypto?articleID="; 
+import { AllPost } from "./data/cryptoBlogData";
 
 export const Container = styled.div`
-
-    background-color: #759ce6;
-    border: 1px black #525252;
-    height: 3270px;
-    padding-top: 15px;
-
-`
+  background-color: #759ce6;
+  border: 1px black #525252;
+  height: 3270px;
+  padding-top: 15px;
+`;
 
 export const Header = styled.div`
+  background-color: #20448a;
+  color: white;
 
-    background-color: #20448a;
-    color: white;
+  height: 140px;
+  border: 1px #525252 solid;
 
-    height: 140px;
-    border: 1px #525252 solid;
+  margin-top: -10px;
 
-    margin-top: -10px;
+  h2 {
+    margin-left: 15px;
+    font-size: 50px;
+  }
 
-    h2 { 
-        margin-left: 15px;
-        font-size: 50px;
-    }
-
-    p {
-        font-size: 40px;
-        text-align: right;
-        margin-top: -92px;
-        margin-right: 30px;
-    }
-
-`
+  p {
+    font-size: 40px;
+    text-align: right;
+    margin-top: -92px;
+    margin-right: 30px;
+  }
+`;
 
 export const BlogPostTitles = styled.div`
+  background-color: black;
+  color: white;
 
-    background-color: black;
-    color: white;
+  opacity: 0.7;
 
-    opacity: 0.7;
+  padding-top: 1px;
+  padding-bottom: 1px;
 
-    padding-top: 1px;
-    padding-bottom: 1px;
-    
-    width: 100%;
+  width: 100%;
 
-    font-size: 25px;
-    margin-top: 190px;
-
-
-`
+  font-size: 25px;
+  margin-top: 190px;
+`;
 
 export const BlogPost = styled.div`
+  posistion: realtive;
 
-    posistion: realtive; 
+  text-align: center;
+  width: 75%;
 
-    text-align: center;
-    width: 75%;
+  height: 350px;
+  border: 1px black solid;
+  border-radius: 5px;
 
-    height: 350px;
-    border: 1px black solid;
-    border-radius: 5px;
+  margin-left: 12%;
+  margin-top: 10%;
 
-    margin-left: 12%;
-    margin-top: 10%;
+  background-image: url(${(props) => props.Image});
+  background-size: 100% 100%;
+`;
 
-    background-image: url(${props => props.Image});
-    background-size: 100% 100%;
-    
-`
+export default function MoblieCrypto() {
+  const [veiwingPage, setVeiwingPage] = useState(false);
 
-export default function MoblieCrypto () {
+  const [pageTitle, setPageTitle] = useState("Title");
+  const [pageDate, setPageDate] = useState("Date");
+  const [pageContent, setPageContent] = useState(AllPost.DeFiCurrentState);
 
-    const [veiwingPage, setVeiwingPage] = useState(false);
+  useEffect(() => {
+    var Temp = window.location.href.split("=");
+    var id = Temp[1];
 
-    const [pageTitle, setPageTitle] = useState("Title");
-    const [pageDate, setPageDate] = useState("Date");
-    const [pageContent, setPageContent] = useState(AllPost.DeFiCurrentState);
-
-
-    useEffect(() => {
-
-        var Temp = window.location.href.split("=");
-        var id = Temp[1];
-
-        if (id) {
-            updatePageById(parseInt(id, 10))
-        }
-    }, [])
-
-    function updatePageById(id){
-       
-        var update = AllPost.map(function (data) {
-            if (data.id == id) {
-
-                setPageTitle(data.title);
-                setPageDate(data.date);
-                setPageContent(data.content);
-                
-                setVeiwingPage(true);
-                window.scrollTo(0, 0)
-     
-              }    
-            });
-
-        return (update);
+    if (id) {
+      updatePageById(parseInt(id, 10));
     }
+  }, []);
 
+  function updatePageById(id) {
+    var update = AllPost.map(function (data) {
+      if (data.id == id) {
+        setPageTitle(data.title);
+        setPageDate(data.date);
+        setPageContent(data.content);
 
-    if (veiwingPage) {
-        return (
-            <>
-            <Header>
-                <h2> Crypto Blog </h2>
-                <p> V1 </p>
-            </Header>
+        setVeiwingPage(true);
+        window.scrollTo(0, 0);
+      }
+    });
 
-            <PageContent title={pageTitle} date={pageDate} content={pageContent} fontSize="30px" headerSize="50px" />
+    return update;
+  }
 
-            <Footer />
-            </>
-        );
-    } else {
-        return (
-            <>
-            <Header>
-                <h2> Crypto Blog </h2>
-                <p> V1 </p>
-            </Header>
+  if (veiwingPage) {
+    return (
+      <>
+        <Header>
+          <h2> Crypto Blog </h2>
+          <p> V1 </p>
+        </Header>
 
-            <Container>
-    
-            {AllPost.map( data => (
-                <BlogPost Image={data.image} 
-                          onClick={() =>                             
-                            window.location = URLlink + data.id
-                        } >
-    
-                    <BlogPostTitles fontSize="14px"
-                                    >
-                        <h2>{data.title}</h2>
-                        <p>{data.date}</p>
-                    </BlogPostTitles>
-    
-                </BlogPost>
-            ))}
+        <PageContent
+          title={pageTitle}
+          date={pageDate}
+          content={pageContent}
+          fontSize="30px"
+          headerSize="50px"
+        />
 
-            </Container>
         <Footer />
-            </>
-        );
-    }
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Header>
+          <h2> Crypto Blog </h2>
+          <p> V1 </p>
+        </Header>
+
+        <Container>
+          {AllPost.map((data) => (
+            <BlogPost
+              Image={data.image}
+              onClick={() => (window.location = "?articleID=" + data.id)}
+            >
+              <BlogPostTitles fontSize="14px">
+                <h2>{data.title}</h2>
+                <p>{data.date}</p>
+              </BlogPostTitles>
+            </BlogPost>
+          ))}
+        </Container>
+        <Footer />
+      </>
+    );
+  }
 }
